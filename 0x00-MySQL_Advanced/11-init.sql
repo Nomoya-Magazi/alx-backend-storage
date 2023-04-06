48ca828524916eaebc41deda06754ec1c826a55a
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2023-04-06 12:38:14 +0200

    Update 11-init.sql

--- 0x00-MySQL_Advanced/11-init.sql
+++ 0x00-MySQL_Advanced/11-init.sql
@@ -13,3 +13,4 @@ INSERT INTO students (name, score) VALUES ("Jean", 60);
 INSERT INTO students (name, score) VALUES ("Steeve", 50);
 INSERT INTO students (name, score) VALUES ("Camilia", 80);
 INSERT INTO students (name, score) VALUES ("Alexa", 130);
+mmm
